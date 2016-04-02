f40cf56b5f4a4e2105e136db71697fb4c74bdd0f
Adam Hunt <avh17@uclive.ac.nz>
2016-04-03 11:25:41 +1200

    Added a multitexture block

--- src/main/java/com/PolarBear505/MyMod/block/ModBlocks.java
+++ src/main/java/com/PolarBear505/MyMod/block/ModBlocks.java
@@ -7,8 +7,10 @@ import net.minecraft.block.material.Material;
 public final class ModBlocks {
 
     public static Block tutorialBlock;
+    public static Block multitexture;
 
     public static final void init() {
         GameRegistry.registerBlock(tutorialBlock = new BasicBlock("blueGlowstone", Material.iron), "blueGlowstone");
+        GameRegistry.registerBlock(multitexture = new MultiTextureBlock("face", Material.cloth), "face");
     }
 }
---
+++ src/main/java/com/PolarBear505/MyMod/block/MultiTextureBlock.java
@@ -0,0 +1,39 @@
+package com.PolarBear505.MyMod.block;
+
+import com.PolarBear505.MyMod.Main;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.util.IIcon;
+
+/**
+ * Created by Adam on 3/04/2016.
+ */
+public class MultiTextureBlock extends Block {
+
+    public IIcon[] icons = new IIcon[6];
+
+    protected MultiTextureBlock(String unlocalizedName, Material material)
+    {
+        super(material);
+        this.setBlockName(unlocalizedName);
+        this.setBlockTextureName(Main.MODID + ":" + unlocalizedName);
+        this.setCreativeTab(CreativeTabs.tabBlock);
+        this.setHardness(2.0F);
+        this.setResistance(6.0F);
+        this.setStepSound(soundTypeGravel);
+    }
+
+    @Override
+    public void registerBlockIcons(IIconRegister reg) {
+        for (int i = 0; i < 6; i ++) {
+            this.icons[i] = reg.registerIcon(this.textureName + "_" + i);
+        }
+    }
+
+    @Override
+    public IIcon getIcon(int side, int meta) {
+        return this.icons[side];
+    }
+}
